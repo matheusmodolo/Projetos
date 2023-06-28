2f975b256d2692a84e171428e2fdbb165b3415af
Matheus Módolo <nmmatheus@hotmail.com>
2023-06-28 00:23:57 -0300

    Implementações e CSS

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,3 +1,29 @@
-function calcular(tipo, valor){
+let historico = document.getElementById("historico");
+let expressao = "";
+
+function calcular(tipo, valor) {
     console.log(tipo, valor);
+
+    if (tipo === "acao") {
+        if (valor === "c") {
+            document.getElementById("tela").value = "";
+            expressao = "";
+        }
+
+        if (valor === "+" || valor === "-" || valor === "*" || valor === "/" || valor === ".") {
+            document.getElementById("tela").value += valor;
+            expressao += valor;
+        }
+
+        if (valor === "=") {
+            var valorDisplay = eval(document.getElementById("tela").value);
+            document.getElementById("tela").value = valorDisplay;
+            historico.innerHTML += "<p>" + expressao + " = " + valorDisplay + "</p>";
+            expressao = valorDisplay;
+        }
+
+    } else {
+        document.getElementById("tela").value += valor;
+        expressao += valor;
+    }
 }
