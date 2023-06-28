b0916f891266fb50b066be714119a086e5400235
Matheus Módolo <nmmatheus@hotmail.com>
2023-06-28 19:41:27 -0300

    Implementação do teclado e alguns ajustes

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,8 +1,73 @@
 let historico = document.getElementById("historico");
 let expressao = "";
 
+function teclado(event) {
+    var tecla = event.key;
+
+    switch (tecla) {
+        case "0":
+            calcular('valor', 0);
+            break;
+        case "1":
+            calcular('valor', 1);
+            break;
+        case "2":
+            calcular('valor', 2);
+            break;
+        case "3":
+            calcular('valor', 3);
+            break;
+        case "4":
+            calcular('valor', 4);
+            break;
+        case "5":
+            calcular('valor', 5);
+            break;
+        case "6":
+            calcular('valor', 6);
+            break;
+        case "7":
+            calcular('valor', 7);
+            break;
+        case "8":
+            calcular('valor', 8);
+            break;
+        case "9":
+            calcular('valor', 9);
+            break;
+        case "+":
+            calcular('acao', '+');
+            break;
+        case "-":
+            calcular('acao', '-');
+            break;
+        case "*":
+            calcular('acao', '*');
+            break;
+        case "/":
+            calcular('acao', '/');
+            break;
+        case "Enter":
+            calcular('acao', '=');
+            break;
+        case "=":
+            calcular('acao', '=');
+            break;
+        case ".":
+            calcular('acao', '.');
+            break;
+        case ",":
+            calcular('acao', '.');
+            break;
+        case " ":
+            calcular('acao', 'c');
+            break;
+        default:
+            break;
+    }
+}
+
 function calcular(tipo, valor) {
-    console.log(tipo, valor);
 
     if (tipo === "acao") {
         if (valor === "c") {
@@ -19,7 +84,9 @@ function calcular(tipo, valor) {
             var valorDisplay = eval(document.getElementById("tela").value);
             document.getElementById("tela").value = valorDisplay;
             historico.innerHTML += "<p>" + expressao + " = " + valorDisplay + "</p>";
-            expressao = valorDisplay;
+
+            //expressao = valorDisplay;
+            expressao = document.getElementById("tela").value;
             document.getElementById("botaoLimpar").style.display = "block";
         }
 
@@ -27,8 +94,6 @@ function calcular(tipo, valor) {
         document.getElementById("tela").value += valor;
         expressao += valor;
     }
-
-
 }
 
 function limparHistorico() {
@@ -38,6 +103,4 @@ function limparHistorico() {
     if (historico.innerHTML === "") {
         document.getElementById("botaoLimpar").style.display = "none";
     }
-
-
 }
